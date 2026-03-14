c9453de6b3bd320842f9f778876daf3627ee5f3f
Nick Marks <nmarks99@gmail.com>
2026-03-14 12:00:04 -0500

    updating inputx and renaming it to monitor

---
+++ apps/monitor.cpp
@@ -0,0 +1,102 @@
+#include <cstdlib>
+#include <pv/caProvider.h>
+#include <pva/client.h>
+
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/loop.hpp>
+#include <ftxui/component/event.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/color.hpp>
+
+#include <pvtui/pvtui.hpp>
+
+using namespace ftxui;
+using namespace pvtui;
+
+static constexpr std::string_view CLI_HELP_MSG = R"(
+pvtui_monitor - Terminal UI for monitoring several PVs.
+
+Usage:
+  pvtui_monitor [options]
+
+Options:
+  -h, --help        Show this help message and exit.
+  -m, --macro       Macros to pass to the UI
+
+Examples:
+    #  Optional prefix followed by list of PVs to monitor
+    pvtui_monitor --macro "P=xxx:" m1.DESC m1.RBV m2.DESC m2.RBV
+
+For more details, visit: https://github.com/BCDA-APS/pvtui
+)";
+
+int main(int argc, char *argv[]) {
+    App app(argc, argv);
+    if (app.args.help(CLI_HELP_MSG)) return EXIT_SUCCESS;
+
+    auto pos_args = app.args.positional_args();
+    if (pos_args.size() < 2) {
+        std::cout << "Please provide PVs to monitor\n";
+        std::cout << CLI_HELP_MSG;
+        return 0;
+    }
+    std::vector<std::string> pv_names(pos_args.begin()+1, pos_args.end());
+
+    // add prefix to PV names if P macro given
+    if (app.args.macros_present({"P"})) {
+        const std::string prefix = app.args.macros.at("P");
+        std::transform(pv_names.begin(), pv_names.end(), pv_names.begin(), [&](auto& s){
+            return prefix + s;
+        });
+    }
+
+    // std::vector<std::unique_ptr<WidgetBase>> widgets;
+    std::vector<std::unique_ptr<Monitor<std::string>>> widgets;
+    if (app.args.flag("edit")) {
+        std::cout << "edit mode not implemented\n";
+        return 0;
+    } else {
+        for (auto& name : pv_names) {
+            widgets.emplace_back(std::make_unique<Monitor<std::string>>(app, name));
+        }
+    }
+
+    auto main_contianer = Container::Vertical({});
+    for (auto& widget : widgets) {
+        main_contianer->Add(widget->component());
+    }
+
+    const auto longest = std::max_element(pv_names.begin(),
+            pv_names.end(), [](const std::string& a, const std::string& b){
+        return a.length() < b.length();
+    });
+
+    auto main_renderer = Renderer(main_contianer, [&] {
+        Elements rows {
+            hbox({
+                text("PV")  | size(WIDTH, EQUAL, longest->size()) | bold | italic,
+                separator(),
+                text("Value") | bold | italic,
+            }),
+            separator()
+        };
+        for (auto& widget : widgets) {
+            rows.push_back(hbox({
+                text(widget->pv_name()) | size(WIDTH, EQUAL, longest->size()),
+                separator(),
+                widget->component()->Render() | color(Color::RGB(125, 174, 227))
+            }));
+            rows.push_back(separator());
+        }
+        return vbox({
+            separator(),
+            vbox({
+                rows
+            }),
+        }) | size(WIDTH, EQUAL, 50);
+    });
+
+    app.run(main_renderer);
+
+}
